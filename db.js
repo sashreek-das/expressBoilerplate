254e804faf22b11fb0737297eba20c72dc39e550
cosmic-wanderer <sashreek102003@gmail.com>
2024-12-28 13:48:10 +0530

    docker file added

--- db.js
+++ db.js
@@ -1,55 +1,55 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
 
-// Load environment variables from .env file
-dotenv.config();
+// // Load environment variables from .env file
+// dotenv.config();
 
-// MongoDB connection URI from environment variables
-const uri = process.env.MONGO_URI;
+// // MongoDB connection URI from environment variables
+// const uri = process.env.MONGO_URI;
 
-mongoose.connect(uri, {
-})
-    .then(() => console.log("Successfully connected to MongoDB"))
-    .catch((err) => console.error("Error connecting to MongoDB:", err));
+// mongoose.connect(uri, {
+// })
+//     .then(() => console.log("Successfully connected to MongoDB"))
+//     .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-// User Schema
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    firstname: {
-        type: String,
-        required: true
-    },
-    lastname: {
-        type: String,
-        required: true
-    }
-});
+// // User Schema
+// const userSchema = new mongoose.Schema({
+//     username: {
+//         type: String,
+//         required: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     },
+//     firstname: {
+//         type: String,
+//         required: true
+//     },
+//     lastname: {
+//         type: String,
+//         required: true
+//     }
+// });
 
-// Question Schema
-const questionSchema = new mongoose.Schema({
-    question: {
-        type: String,
-        required: true
-    },
-    answer: {
-        type: String,
-        required: true
-    }
-});
+// // Question Schema
+// const questionSchema = new mongoose.Schema({
+//     question: {
+//         type: String,
+//         required: true
+//     },
+//     answer: {
+//         type: String,
+//         required: true
+//     }
+// });
 
-// Models
-const User = mongoose.model("User", userSchema);
-const Question = mongoose.model("Question", questionSchema);
+// // Models
+// const User = mongoose.model("User", userSchema);
+// const Question = mongoose.model("Question", questionSchema);
 
-// Export models
-module.exports = {
-    User,
-    Question
-};
+// // Export models
+// module.exports = {
+//     User,
+//     Question
+// };
